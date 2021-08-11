cb60681e4bbaf91bd81748f0c74541369e9f48ac
walrus10000 <anon51102@gmail.com>
2021-08-11 16:16:27 +0200

    Update

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,8 +24,10 @@ namespace API
             {
                 options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
-            
+
             services.AddControllers();
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +41,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
